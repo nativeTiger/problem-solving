a5ea04c1321701b2dcca72c13dca969e54523350
nativeTiger <santakumal41@gmail.com>
2022-07-07 21:42:33 +0545

    ratio of the numbers of array

--- js/Easy/index.js
+++ js/Easy/index.js
@@ -2,3 +2,5 @@ export { default as simpleArraySum } from "./simpleArraySum.js";
 export { default as prime } from "./primeNumber.js";
 export { default as stairCase } from "./staircase.js";
 export { default as minMax } from "./minMax.js";
+export { default as noOfTallestCandles } from "./candlesProblem.js";
+export { default as plusMinusRatio } from "./plusMinus.js";
--- js/index.js
+++ js/index.js
@@ -1,4 +1,11 @@
-import { simpleArraySum, prime, stairCase, minMax } from "./Easy/index.js";
+import {
+  simpleArraySum,
+  prime,
+  stairCase,
+  minMax,
+  noOfTallestCandles,
+  plusMinusRatio,
+} from "./Easy/index.js";
 
 // simpleArraySum();
 
@@ -6,6 +13,10 @@ import { simpleArraySum, prime, stairCase, minMax } from "./Easy/index.js";
 
 // stairCase(10);
 
-const { minSum, maxSum } = minMax([1, 2, 3, 4, 5]);
+// const { minSum, maxSum } = minMax([1, 2, 3, 4, 5]);
 
-console.log(minSum, maxSum);
+// console.log(minSum, maxSum);
+
+console.log(noOfTallestCandles([1, 2, 3, 4, 5, 6, 6, 6]));
+
+plusMinusRatio([1, 1, 0, -1, -1]);
