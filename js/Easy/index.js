7c0a63ae3c84db7f85edbbaf6b16850264628039
nativeTiger <santakumal41@gmail.com>
2022-07-07 20:00:32 +0545

    min max

--- js/Easy/index.js
+++ js/Easy/index.js
@@ -1,2 +1,4 @@
 export { default as simpleArraySum } from "./simpleArraySum.js";
 export { default as prime } from "./primeNumber.js";
+export { default as stairCase } from "./staircase.js";
+export { default as minMax } from "./minMax.js";
--- js/index.js
+++ js/index.js
@@ -1,5 +1,11 @@
-import { simpleArraySum, prime } from "./Easy/index.js";
+import { simpleArraySum, prime, stairCase, minMax } from "./Easy/index.js";
 
-simpleArraySum();
+// simpleArraySum();
 
-console.log(prime.primeArray);
+// console.log(prime.primeArray);
+
+// stairCase(10);
+
+const { minSum, maxSum } = minMax([1, 2, 3, 4, 5]);
+
+console.log(minSum, maxSum);
